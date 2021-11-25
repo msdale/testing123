04c6a84eddf487f1d2b2056b080d6172c66e27e3
Mark Dale <msdaledad@gmail.com>
2021-11-24 20:25:47 -0500

    more stuff

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,4 @@
-fetch("https://dev.virtualearth.net/REST/v1/Routes?waypoint.1=4400+Baptist+Island+Rd+Groveland+FL&waypoint.2=Orlando+Fl&distanceUnit=mi&o=json&c=en-GB&key=ApeHivnaztSwvGsBzGpbwnYJw9vAPSBI4LrGUh_YRYvmqZVRsrxjevRoSXGWi7M8")
+/*fetch("https://dev.virtualearth.net/REST/v1/Routes?waypoint.1=4400+Baptist+Island+Rd+Groveland+FL&waypoint.2=Orlando+Fl&distanceUnit=mi&o=json&c=en-GB&key=ApeHivnaztSwvGsBzGpbwnYJw9vAPSBI4LrGUh_YRYvmqZVRsrxjevRoSXGWi7M8")
 .then(response => {
 	console.log(response);
 })
@@ -7,10 +7,19 @@ fetch("https://dev.virtualearth.net/REST/v1/Routes?waypoint.1=4400+Baptist+Islan
 });
 
 fetch("https://target-com-store-product-reviews-locations-data.p.rapidapi.com/location/search?zip=34736&radius=100", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "target-com-store-product-reviews-locations-data.p.rapidapi.com",
-		"x-rapidapi-key": "9813878aa1msh7c70fcdc9bbf8a6p1e1f78jsn1ff70bded748"
+	"method": "GET",const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://eventbrite-com.p.rapidapi.com/event/117496833191",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "eventbrite-com.p.rapidapi.com",
+      "x-rapidapi-key": "SIGN-UP-FOR-KEY"
+    }
+  };
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });7c70fcdc9bbf8a6p1e1f78jsn1ff70bded748"
 	}
 })
 .then(response => {
@@ -46,4 +55,48 @@ fetch("https://walmart3.p.rapidapi.com/store-location?zip_code=34736", {
 })
 .catch(err => {
 	console.error(err);
+});
+
+fetch("https://eventbrite-com.p.rapidapi.com/event/117496833191", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "eventbrite-com.p.rapidapi.com",
+		"x-rapidapi-key": "9813878aa1msh7c70fcdc9bbf8a6p1e1f78jsn1ff70bded748"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});*/
+
+const settings1 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+		"x-rapidapi-key": "afd438218bmshadd32e5982f2dbbp107b44jsn656d7e59884b"
+	}
+};
+
+$.ajax(settings1).done(function (response) {
+	console.log(response);
+});
+
+const settings2 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "imdb8.p.rapidapi.com",
+		"x-rapidapi-key": "afd438218bmshadd32e5982f2dbbp107b44jsn656d7e59884b"
+	}
+};
+
+$.ajax(settings2).done(function (response) {
+	console.log(response);
 });
